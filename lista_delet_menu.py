87e5a1310fcf17c08261d9c47792d8abc99773e8
rafaeltaffo <rafael.taffo07@gmail.com>
2026-05-29 00:02:44 -0300

    feat

--- lista_delet_menu.py
+++ lista_delet_menu.py
@@ -7,3 +7,29 @@ def menu_list():            #Lista os pokemon cadastrados
         for p in pokedex:
             print(f"ID: {p.id} | Nome: {p.nome} | Tipo: {p.tipo} | Nível: {p.nivel}")
     voltar()
+
+def menu_del():
+    limpar_term()
+    print("--- REMOVER POKEMON ---\n")
+    if not pokedex:
+        print("Não há Pokémon cadastrado...")
+    else:
+        for p in pokedex:
+            print("\nID: " + str(p.id) + "\nNome: " + p.nome)
+
+        try:
+            id_del = int(input("\nQual Pokémon (ID) você deseja remover?\n"))
+            encontrado = False
+
+            for p in pokedex:
+                if id_del == p.id:
+                    encontrado = True
+                    pokedex.remove(p)
+                    save()
+                    print(f"{p.nome} - {p.id}\nExcluído com sucesso!")
+                    break
+            if not encontrado:
+                print("Pokemon não encontrado...")
+        except ValueError:
+            print("Digite um ID válido.")
+    voltar()
